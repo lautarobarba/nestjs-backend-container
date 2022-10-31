9e3300e66abf3d81940791e782ccd070a9c7e03f
Lautaro Barba <lau_mb94@hotmail.com>
2022-10-31 13:14:24 -0300

    Se agregaron logs

--- backend/src/modules/mailer/mailer.service.ts
+++ backend/src/modules/mailer/mailer.service.ts
@@ -1,30 +1,30 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { Job, Queue } from 'bull';
 import { InjectQueue, Process, Processor } from '@nestjs/bull';
 import { MailerService as MailerServiceNode } from '@nestjs-modules/mailer';
 import { UserService } from 'modules/user/user.service';
 import { User } from 'modules/user/user.entity';
-import { AuthService } from 'modules/auth/auth.service';
-import { SessionDto } from 'modules/auth/auth.dto';
 
 @Processor('emailSender')
 @Injectable()
 export class MailerService {
 	constructor(
-		@InjectQueue('emailSender') 
+		@InjectQueue('emailSender')
 		private readonly _emailSenderQueue: Queue,
 		private readonly _mailerServiceNode: MailerServiceNode,
 		private readonly _userService: UserService,
-		) {}
+	) { }
+	private readonly _logger = new Logger(MailerService.name);
 
 	// Envía la tarea 'handleSendTestEmail' a la cola
 	async sendTestEmail(
-		ulrToImportCssInEmail: string, 
+		ulrToImportCssInEmail: string,
 		ulrToImportImagesInEmail: string,
 		userEmail: string,
 		overwriteEmail?: string
-		) {
-		console.log(`Se enviará un correo de prueba a ${userEmail}`);
+	) {
+		this._logger.debug('sendTestEmail()');
+		this._logger.debug(`Se enviará un correo de prueba a ${userEmail}`);
 
 		const job: Job = await this._emailSenderQueue.add('handleSendTestEmail', {
 			ulrToImportCssInEmail: ulrToImportCssInEmail,
@@ -41,8 +41,10 @@ export class MailerService {
 	// Ejecula la próxima tarea 'handleSendTestEmail' de la cola
 	@Process('handleSendTestEmail')
 	async handleSendTestEmail(job: Job) {
-		const { ulrToImportCssInEmail,  ulrToImportImagesInEmail, mailbox } = job.data;
-		console.log(`handleSendTestEmail: BEGIN Enviando correo a- ${mailbox}`);
+		this._logger.debug('handleSendTestEmail()');
+		const { ulrToImportCssInEmail, ulrToImportImagesInEmail, mailbox } = job.data;
+		this._logger.debug(`handleSendTestEmail: BEGIN Enviando correo a- ${mailbox}`);
+
 		try {
 			await this._mailerServiceNode.sendMail({
 				to: mailbox,
@@ -55,24 +57,25 @@ export class MailerService {
 					mailbox: mailbox
 				},
 			});
-			console.log(`handleSendTestEmail: END Enviando correo a- ${mailbox}`);
-		} catch (error){
-			console.log(`handleSendTestEmail: ERROR Enviando correo a- ${mailbox}`);
+			this._logger.debug(`handleSendTestEmail: END Enviando correo a- ${mailbox}`);
+		} catch (error) {
+			this._logger.debug(`handleSendTestEmail: ERROR Enviando correo a- ${mailbox}`);
 			console.log(error);
 		}
 	}
 
 	// Envía la tarea 'handleSendRegistrationEmail' a la cola
 	async sendRegistrationEmail(
-		ulrToImportCssInEmail: string, 
-		ulrToImportImagesInEmail: string, 
-		userEmail: string, 
+		ulrToImportCssInEmail: string,
+		ulrToImportImagesInEmail: string,
+		userEmail: string,
 		overwriteEmail?: string
-		) {
+	) {
+		this._logger.debug('sendRegistrationEmail()');
 		const user: User = await this._userService.findOneByEmail(userEmail);
 
-		if(!user) throw new NotFoundException('User does not exists');
-		
+		if (!user) throw new NotFoundException('User does not exists');
+
 		// console.log(user.email);
 
 		const job: Job = await this._emailSenderQueue.add('handleSendRegistrationEmail', {
@@ -90,9 +93,11 @@ export class MailerService {
 
 	// Ejecula la próxima tarea 'handleSendRegistrationEmail' de la cola
 	@Process('handleSendRegistrationEmail')
-  async handleSendRegistrationEmail(job: Job) {
-		const { ulrToImportCssInEmail,  ulrToImportImagesInEmail, user, mailbox } = job.data;
-		console.log(`handleSendRegistrationEmail: BEGIN Enviando correo a- ${mailbox}`);
+	async handleSendRegistrationEmail(job: Job) {
+		this._logger.debug('handleSendRegistrationEmail()');
+		const { ulrToImportCssInEmail, ulrToImportImagesInEmail, user, mailbox } = job.data;
+		this._logger.debug(`handleSendRegistrationEmail: BEGIN Enviando correo a- ${mailbox}`);
+
 		try {
 			await this._mailerServiceNode.sendMail({
 				to: mailbox,
@@ -105,25 +110,26 @@ export class MailerService {
 					user: user
 				},
 			});
-			console.log(`handleSendRegistrationEmail: END Enviando correo a- ${mailbox}`);
-		} catch (error){
-			console.log(`handleSendRegistrationEmail: ERROR Enviando correo a- ${mailbox}`);
+			this._logger.debug(`handleSendRegistrationEmail: END Enviando correo a- ${mailbox}`);
+		} catch (error) {
+			this._logger.debug(`handleSendRegistrationEmail: ERROR Enviando correo a- ${mailbox}`);
 			console.log(error);
 		}
-  }
+	}
 
 	// Envía la tarea 'handleSendEmailConfirmationEmail' a la cola
 	async sendEmailConfirmationEmail(
-		ulrToImportCssInEmail: string, 
-		ulrToImportImagesInEmail: string, 
+		ulrToImportCssInEmail: string,
+		ulrToImportImagesInEmail: string,
 		userEmail: string,
 		accessToken: string,
 		overwriteEmail?: string
-		) {
+	) {
+		this._logger.debug('sendEmailConfirmationEmail()');
 		const user: User = await this._userService.findOneByEmail(userEmail);
 
-		if(!user) throw new NotFoundException('User does not exists');
-		
+		if (!user) throw new NotFoundException('User does not exists');
+
 		// console.log(user.email);
 
 		const job: Job = await this._emailSenderQueue.add('handleSendEmailConfirmationEmail', {
@@ -143,8 +149,9 @@ export class MailerService {
 	// Ejecula la próxima tarea 'handleSendEmailConfirmationEmail' de la cola
 	@Process('handleSendEmailConfirmationEmail')
 	async handleSendEmailConfirmationEmail(job: Job) {
-		const { ulrToImportCssInEmail,  ulrToImportImagesInEmail, user, accessToken, mailbox } = job.data;
-		console.log(`handleSendEmailConfirmationEmail: BEGIN Enviando correo a- ${mailbox}`);
+		this._logger.debug('handleSendEmailConfirmationEmail()');
+		const { ulrToImportCssInEmail, ulrToImportImagesInEmail, user, accessToken, mailbox } = job.data;
+		this._logger.debug(`handleSendEmailConfirmationEmail: BEGIN Enviando correo a- ${mailbox}`);
 
 		// Ruta para confirmar el correo electrónico en el frontend
 		const emailConfirmationUrl: string = `${process.env.EMAIL_CONFIRMATION_URL}/${accessToken}`;
@@ -162,24 +169,25 @@ export class MailerService {
 					emailConfirmationUrl
 				},
 			});
-			console.log(`handleSendEmailConfirmationEmail: END Enviando correo a- ${mailbox}`);
-		} catch (error){
-			console.log(`handleSendEmailConfirmationEmail: ERROR Enviando correo a- ${mailbox}`);
+			this._logger.debug(`handleSendEmailConfirmationEmail: END Enviando correo a- ${mailbox}`);
+		} catch (error) {
+			this._logger.debug(`handleSendEmailConfirmationEmail: ERROR Enviando correo a- ${mailbox}`);
 			console.log(error);
 		}
 	}
 
 	// Envía la tarea 'handleSendEmailConfirmedEmail' a la cola
 	async sendEmailConfirmedEmail(
-		ulrToImportCssInEmail: string, 
-		ulrToImportImagesInEmail: string, 
+		ulrToImportCssInEmail: string,
+		ulrToImportImagesInEmail: string,
 		userEmail: string,
 		overwriteEmail?: string
-		) {
+	) {
+		this._logger.debug('sendEmailConfirmedEmail()');
 		const user: User = await this._userService.findOneByEmail(userEmail);
 
-		if(!user) throw new NotFoundException('User does not exists');
-		
+		if (!user) throw new NotFoundException('User does not exists');
+
 		// console.log(user.email);
 
 		const job: Job = await this._emailSenderQueue.add('handleSendEmailConfirmedEmail', {
@@ -198,8 +206,9 @@ export class MailerService {
 	// Ejecula la próxima tarea 'handleSendEmailConfirmedEmail' de la cola
 	@Process('handleSendEmailConfirmedEmail')
 	async handleSendEmailConfirmedEmail(job: Job) {
-		const { ulrToImportCssInEmail,  ulrToImportImagesInEmail, user, accessToken, mailbox } = job.data;
-		console.log(`handleSendEmailConfirmedEmail: BEGIN Enviando correo a- ${mailbox}`);
+		this._logger.debug('handleSendEmailConfirmedEmail()');
+		const { ulrToImportCssInEmail, ulrToImportImagesInEmail, user, accessToken, mailbox } = job.data;
+		this._logger.debug(`handleSendEmailConfirmedEmail: BEGIN Enviando correo a- ${mailbox}`);
 
 		try {
 			await this._mailerServiceNode.sendMail({
@@ -213,25 +222,26 @@ export class MailerService {
 					user: user,
 				},
 			});
-			console.log(`handleSendEmailConfirmedEmail: END Enviando correo a- ${mailbox}`);
-		} catch (error){
-			console.log(`handleSendEmailConfirmedEmail: ERROR Enviando correo a- ${mailbox}`);
+			this._logger.debug(`handleSendEmailConfirmedEmail: END Enviando correo a- ${mailbox}`);
+		} catch (error) {
+			this._logger.debug(`handleSendEmailConfirmedEmail: ERROR Enviando correo a- ${mailbox}`);
 			console.log(error);
 		}
 	}
 
 	// Envía la tarea 'handleSendRecoverPasswordEmail' a la cola
 	async sendRecoverPasswordEmail(
-		ulrToImportCssInEmail: string, 
-		ulrToImportImagesInEmail: string, 
+		ulrToImportCssInEmail: string,
+		ulrToImportImagesInEmail: string,
 		userEmail: string,
 		accessToken: string,
 		overwriteEmail?: string
-		) {
+	) {
+		this._logger.debug('sendRecoverPasswordEmail()');
 		const user: User = await this._userService.findOneByEmail(userEmail);
 
-		if(!user) throw new NotFoundException('User does not exists');
-		
+		if (!user) throw new NotFoundException('User does not exists');
+
 		// console.log(user.email);
 
 		const job: Job = await this._emailSenderQueue.add('handleSendRecoverPasswordEmail', {
@@ -251,8 +261,9 @@ export class MailerService {
 	// Ejecula la próxima tarea 'handleSendRecoverPasswordEmail' de la cola
 	@Process('handleSendRecoverPasswordEmail')
 	async handleSendRecoverPasswordEmail(job: Job) {
-		const { ulrToImportCssInEmail,  ulrToImportImagesInEmail, user, accessToken, mailbox } = job.data;
-		console.log(`handleSendRecoverPasswordEmail: BEGIN Enviando correo a- ${mailbox}`);
+		this._logger.debug('sendRecoverPasswordEmail()');
+		const { ulrToImportCssInEmail, ulrToImportImagesInEmail, user, accessToken, mailbox } = job.data;
+		this._logger.debug(`handleSendRecoverPasswordEmail: BEGIN Enviando correo a- ${mailbox}`);
 
 		// Ruta para confirmar el correo electrónico en el frontend
 		const passwordRecoveryUrl: string = `${process.env.EMAIL_RECOVERY_URL}/${accessToken}`;
@@ -270,9 +281,9 @@ export class MailerService {
 					passwordRecoveryUrl
 				},
 			});
-			console.log(`handleSendRecoverPasswordEmail: END Enviando correo a- ${mailbox}`);
-		} catch (error){
-			console.log(`handleSendRecoverPasswordEmail: ERROR Enviando correo a- ${mailbox}`);
+			this._logger.debug(`handleSendRecoverPasswordEmail: END Enviando correo a- ${mailbox}`);
+		} catch (error) {
+			this._logger.debug(`handleSendRecoverPasswordEmail: ERROR Enviando correo a- ${mailbox}`);
 			console.log(error);
 		}
 	}
